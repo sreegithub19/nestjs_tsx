dd8bd638106c99b9f362760f42cd5c9bb0bf6608
K <sreedhar.k@rakuten.com>
2023-04-08 14:29:50 +0530

    Changes

--- src/topic/subindex/subindex.service.ts
+++ src/topic/subindex/subindex.service.ts
@@ -16,7 +16,7 @@ export class TopicService {
         <li><a href='/topic/tic_tac_toe'>Tic-tac-toe</a></li>
         <li><a href='/topic/clock'>Analogue clock</a></li>
         <li><a href='/topic/hangman'>Hangman</a></li>
-        <li><a href='/topic/puzzles'>Estonian puzzles</a></li>
+        <li><a href='/topic/puzzles'>Puzzles</a></li>
         <li><a href='/topic/sudoku'>Sudoku</a></li>
         <li><a href='/topic/virtual_keyboard'>Virtual keyboard</a></li>
 	<li><a href='/topic/solitaire'>Solitaire</a></li>
@@ -2600,7 +2600,6 @@ a:visited {
 }
 
 @media (min-height: 425px) {
-  /** Youtube logo by https://codepen.io/alvaromontoro */
   #youtube {
     z-index: 2;
     display: block;
@@ -2684,20 +2683,7 @@ a:visited {
     </div>
   </div>
 </div>
-<a id="youtube" href="https://youtu.be/bTk6dcAckuI" target="_blank">
-  <span>See how this game was made</span>
-</a>
-<div id="youtube-card">
-  How to simulate ball movement in a maze with JavaScript
-</div>
 <script>
-    /*
-
-If you want to know how this game works, you can find a source code walkthrough video here: https://youtu.be/bTk6dcAckuI
-
-Follow me on twitter for more: https://twitter.com/HunorBorbely
-
-*/
 
 Math.minmax = (value, limit) => {
   return Math.max(Math.min(value, limit), -limit);
@@ -3356,10 +3342,6 @@ function main(timestamp) {
       noteElement.innerHTML = \`Congrats, you did it!
         \${!hardMode ? "<p>Press H for hard mode</p>" : ""}
         <p>
-          Follow me
-          <a href="https://twitter.com/HunorBorbely" , target="_blank"
-            >@HunorBorbely</a
-          >
         </p>\`;
       noteElement.style.opacity = 1;
       gameInProgress = false;
@@ -9612,11 +9594,11 @@ return this;
 
     <script>
     var images = [
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/neeruti.jpg', title: 'Neeruti manor' },
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/harju_madise.jpg', title: 'Harju-Madis Church' },
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/rahumae.jpg', title: 'Rahumäe train station' },
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/kakumae.jpg', title: 'Kakumäe Harbor' },
-        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/kohila.jpg', title: 'Kohila mill' }
+        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/neeruti.jpg', title: 'Puzzle_1' },
+        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/harju_madise.jpg', title: 'Puzzle_2' },
+        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/rahumae.jpg', title: 'Puzzle_3' },
+        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/kakumae.jpg', title: 'Puzzle_4' },
+        { src: 'https://raw.githubusercontent.com/sreegithub19/JavaScript-Applications/main/public/puzzle/img/kohila.jpg', title: 'Puzzle_5' }
     ];
     
     $(function () {
